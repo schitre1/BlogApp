f5499b1cd35f8b7373e052dbb3a050a13df820a5
Chitre <schitre@paypal.com>
2018-11-19 23:42:33 -0800

    Add link and redux-form with baic field

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,36 @@
 import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
-    render(){
+    renderTitleField(field){ //field obj contains some event handlers that we need to wire up to the jsx we are returning
+        //the input we enter should be tracked to the field component
         return (
             <div>
-                PostsNew
+                <input
+                type="text"
+                {...field.input}
+                />
             </div>
+        );
+    }
+    //field.input contains a bunch of different event handlers and a bunch of different props onChnge, onBlur, onFocus, it also has the value of the event
+    //... helps us save trouble of writing onChange: field.input.onChange etc. etc, 
+    render(){
+        return (
+            <form>
+                <Field 
+                name="title" 
+                component={this.renderTitleField}/>
+            </form>
         );
     }
 }
 
-export default PostsNew;
+//component propm should be a function that returns some amount of jsx
+
+export default reduxForm({
+    form: 'PostsNewForm' //multiple forms may exist on same page, make sure the string you assign to the form prop is unique, we don't want merged states bwtween two different forms
+})(PostsNew);
+
+//reduxForm is a function that is similar to connect helper from 'react-redux'
+//reduxForm allows our component to communicate with the additional reducer that we wired in , formReducer
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer_posts';
+import {reducer as formReducer} from 'redux-form';
+
+//as is a keyword, we are basically renaming reducer property in case some other reducer comes into the picture too
 
 const rootReducer = combineReducers({
-    posts: PostsReducer
+    posts: PostsReducer,
+    form: formReducer
 });
 
+//formReducer is going to be applied to the form piece of state everywhere.
 export default rootReducer;
